90a40f274d70546db5f75d425ba6010147b464d9
iscreem <nahuel_1.5@outlook.com>
2022-09-26 23:46:02 -0300

    student and class dao, no intermediate table

--- src/main/java/db/dao/ClassDAO.java
+++ src/main/java/db/dao/ClassDAO.java
@@ -6,58 +6,53 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClassDAO implements IBaseDAO<Class> {
 
     private static final Logger LOGGER = LogManager.getLogger(ClassDAO.class.getName());
-    private final String INSERT = "INSERT INTO Classes(Professors_id,Courses_id,date,Classrooms_id,Subjects_id) " + "VALUES(?,?,?,?,?)";
-    private final String UPDATE = "UPDATE Classes SET Professors_id = ?, Courses_id = ?, date = ?, Classrooms_id = ?, Subjects_id = ?, WHERE id = ?;";
-    private final String DELETE = "DELETE FROM Classes WHERE id = ?";
-    private final String GET_BY_ID = "SELECT * FROM Classes WHERE id = ?";
-    private final String GET_ALL = "SELECT * FROM Classes ORDER BY id";
+    private final String INSERT = "INSERT INTO Classes(professors_id,courses_id,date,classrooms_id,subjects_id) " + "VALUES(?,?,?,?,?);";
+    private final String UPDATE = "UPDATE Classes SET professors_id = ?, courses_id = ?, date = ?, classrooms_id = ?, subjects_id = ?, WHERE id = ?;";
+    private final String DELETE = "DELETE FROM Classes WHERE id = ?;";
+    private final String GET_BY_ID = "SELECT * FROM Classes WHERE id = ?;";
+    private final String GET_ALL = "SELECT * FROM Classes ORDER BY id;";
 
     @Override
     public void insert(Class object) throws SQLException {
         Connection c = ConnectionPool.getInstance().getConnection();
-        PreparedStatement ps = null;
-        try {
-            ps = c.prepareStatement(INSERT);
-//            ps.setInt(1, object.getProfessor().getId());
-//            ps.setInt(2, object.getCourse().getId());
-//            ps.setDate(3, (Date) object.getDate());
-//            ps.setInt(4, object.getClassroom().getId());
-//            ps.setInt(5, object.getSubject().getId());
+        try (PreparedStatement ps = c.prepareStatement(INSERT)) {
+            ps.setInt(1, object.getProfessorId());
+            ps.setInt(2, object.getCourseId());
+            ps.setDate(3, (Date) object.getDate());
+            ps.setInt(4, object.getClassroomId());
+            ps.setInt(5, object.getSubjectId());
             ps.executeUpdate();
-
         } catch (SQLException e) {
-            LOGGER.error(e.getMessage());
+            LOGGER.error("Failed inserting record",e);
         } finally {
-            assert ps != null;
-            ps.close();
             ConnectionPool.getInstance().releaseConnection(c);
         }
-        throw new SQLException("Could not get ID of newly created object");
     }
 
     @Override
     public void update(Class object) throws SQLException {
         Connection c = ConnectionPool.getInstance().getConnection();
-        PreparedStatement ps = null;
-        try {
-            ps = c.prepareStatement(UPDATE);
-//            ps.setInt(1, object.getProfessor().getId());
-//            ps.setInt(2, object.getCourse().getId());
-//            ps.setDate(3, (Date) object.getDate());
-//            ps.setInt(4, object.getClassroom().getId());
-//            ps.setInt(5, object.getSubject().getId());
-//            ps.setInt(6, object.getId());
-            ps.executeUpdate();
+        try (PreparedStatement ps = c.prepareStatement(UPDATE)){
+            ps.setInt(1, object.getProfessorId());
+            ps.setInt(2, object.getCourseId());
+            ps.setDate(3, (Date) object.getDate());
+            ps.setInt(4, object.getClassroomId());
+            ps.setInt(5, object.getSubjectId());
+            ps.setInt(6, object.getId());
+            if(ps.executeUpdate()>0){
+                String message = String.format("Class ID: %d was updated", object.getId());
+                LOGGER.info(message);
+            }
         } catch (SQLException e) {
-            LOGGER.error(e.getMessage());
+            String message = String.format("Couldn't update class ID: %d ", object.getId());
+            LOGGER.error(message, e);
         } finally {
-            assert ps != null;
-            ps.close();
             ConnectionPool.getInstance().releaseConnection(c);
         }
     }
@@ -65,51 +60,69 @@ public class ClassDAO implements IBaseDAO<Class> {
     @Override
     public void delete(int id) throws SQLException {
         Connection c = ConnectionPool.getInstance().getConnection();
-        PreparedStatement ps = null;
-        try{
-            ps = c.prepareStatement(DELETE);
+        try (PreparedStatement ps = c.prepareStatement(DELETE)){
             ps.setInt(1,id);
-            ps.executeUpdate();
-        }catch (SQLException e){
-            LOGGER.error(e);
-        }finally {
-            ConnectionPool.getInstance().releaseConnection(c);
-            try{
-                ps.close();
-            }catch (SQLException e){
-                LOGGER.error(e);
+            if(ps.executeUpdate()>0){
+                String message = String.format("Class ID: %d was deleted", id);
+                LOGGER.info(message);
             }
+        } catch (SQLException e) {
+            String message = String.format("Class ID: %d was not able to be deleted", id);
+            LOGGER.error(message, e);
+        } finally {
+            ConnectionPool.getInstance().releaseConnection(c);
         }
     }
 
     @Override
     public Class getById(int id) throws SQLException {
         Connection c = ConnectionPool.getInstance().getConnection();
-        PreparedStatement ps = null;
         ResultSet rs = null;
-        try {
-            ps = c.prepareStatement(GET_BY_ID);
+        try (PreparedStatement ps = c.prepareStatement(GET_BY_ID)){
             ps.setInt(1, id);
             rs = ps.executeQuery();
-            while (rs.next()) {
-                //Class object = new Class(rs.getInt(1),rs.getInt(2), rs.getInt(3),rs.getDate(4), rs.getInt(5), rs.getInt(6), rs.getInt(7));
-                //object.setId(id);
-                //object.setDate(rs.getDate("date"));
-                return null;
+            if(rs.next()){
+                return parser(rs);
             }
         } catch (SQLException e) {
-            LOGGER.error(e.getMessage());
+            String message = String.format("Failed getting Class ID: %d", id);
+            LOGGER.error(message, e);
         } finally {
             assert rs != null;
             rs.close();
-            ps.close();
             ConnectionPool.getInstance().releaseConnection(c);
         }
-        throw new SQLException("No data matching the ID given");
+        return null;
     }
 
     @Override
     public List<Class> getAll() throws SQLException {
-        return null;
+        Connection c = ConnectionPool.getInstance().getConnection();
+        ResultSet rs = null;
+        ArrayList<Class> classes = new ArrayList<>();
+        try(PreparedStatement ps = c.prepareStatement(GET_ALL)){
+            rs = ps.executeQuery();
+            while (rs.next()){
+                classes.add(parser(rs));
+            }
+        }catch (SQLException e){
+            LOGGER.error("Getting all records from Classes Table Failed", e);
+        }finally {
+            ConnectionPool.getInstance().releaseConnection(c);
+            assert rs != null;
+            rs.close();
+        }
+        return classes;
+    }
+
+    private Class parser(ResultSet rs) throws SQLException {
+        Class clas = new Class();
+        clas.setId(rs.getInt(1));
+        clas.setProfessorId(rs.getInt(2));
+        clas.setCourseId(rs.getInt(3));
+        clas.setDate(rs.getDate(4));
+        clas.setClassroomId(rs.getInt(5));
+        clas.setSubjectId(rs.getInt(6));
+        return clas;
     }
 }
