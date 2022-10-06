c09f4e96f5fea0c61abfa5c9f13ee56aaa3fdf02
iscreem <nahuel_1.5@outlook.com>
2022-10-06 09:05:49 -0300

    added script with aggregate functions

--- script_deletions.sql
+++ script_deletions.sql
@@ -8,3 +8,14 @@ DELETE FROM Exams WHERE Courses_id=3;
 
 DELETE FROM Classes WHERE Courses_id=3;
 
+DELETE FROM Students WHERE id=15;
+
+DELETE FROM Students WHERE firstName='Isis' AND id > 14;
+
+DELETE FROM Students WHERE firstName='Tamera' AND lastName='Frey';
+
+DELETE FROM Grades WHERE grade < 4;
+
+
+
+
